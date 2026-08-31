9a29b3f7152ea66cbaa31326874189e7b9808064
TTRPG Soundboard Developer <developer@ttrpg-soundboard.local>
2026-08-31 03:56:46 +0300

    Initialize Android project configuration and assets

---
+++ src-tauri/gen/android/app/src/main/java/com/mrhorakhty/thespellcaster/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mrhorakhty.thespellcaster
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
